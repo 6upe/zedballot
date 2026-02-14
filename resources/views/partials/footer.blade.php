053b9dbfc590db1459582031958241906318a670
6upe <katongobupe444@gmail.com>
2026-02-14 10:27:22 +0200

    Meta Tags

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,5 +2,5 @@
   <div class="footer-left">
     Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
   </div>
-  <div class="footer-right"></div>
+  <div class="footer-right">Musy Technologies</div>
 </footer>
